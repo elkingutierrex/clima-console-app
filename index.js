7275ba2ec9b628b9d645333e2b8372c4a5a07bc2
elkingutierrex <elkin.gutierrex@gmail.com>
2021-11-24 19:07:10 -0500

    69. Modelo para controlar la aplicación.

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
+
 
 
 const main = async () =>{
+
+    const busquedas = new Busquedas();
 
     let opt;
 
     do{
 
         opt = await inquirerMenu();
-        console.log( { opt });
+
+        switch (opt ) {
+            case 1:
+
+                const lugar = await leerInput( 'Ciudad: ' );
+                
+                console.log( lugar );
+                console.log( '\nInformación de la ciudad\n'.green );
+                console.log( 'Ciudad:'.green );
+                console.log( 'Lat:'.green );
+                console.log( 'Lng'.green );
+                console.log( 'Temp'.green );
+                console.log( 'Min'.green );
+                console.log( 'Max'.green );
+                break;
+            case 2:
+                
+                break;
+            case 0:
+                
+                break;
+        
+            default:
+                break;
+        }
+       
+
         if ( opt !== 0 ) await pausa();
 
     } while (opt !== 0)
---
+++ models/busquedas.js
@@ -0,0 +1,23 @@
+
+
+
+class Busquedas {
+    
+    historial = [ '', '',];
+
+    constructor(){
+        // TODO: leer db si existe
+
+    }
+
+    async ciudad ( lugar = '' ){
+
+        //petición http
+        console.log( lugar );
+
+        return [];
+    }
+
+}
+
+module.exports = Busquedas;
